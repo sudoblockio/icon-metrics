8575fc5e91f225554be9bbb1fd63c0f16d84230c
robcxyz <rob@geometrylabs.io>
2021-11-22 17:30:19 -0800

    chore: fix init in tests

--- tests/integration/worker/test_get_supply.py
+++ tests/integration/worker/test_get_supply.py
@@ -1,10 +1,12 @@
 from sqlmodel import select
 
+from icon_metrics.data.db_init import init_db
 from icon_metrics.models.metrics import Supply
 from icon_metrics.workers.supply_cron import get_supply
 
 
 def test_get_supply(db):
+    init_db()
     with db as session:
         get_supply(session, 0)
         result = session.execute(select(Supply))
