588affa3d8a2afa36a9db68de93fc2bbf075d369
robcxyz <rob@geometrylabs.io>
2022-03-21 20:14:24 -0700

    fix: fix api test

--- tests/api/test_metrics.py
+++ tests/api/test_metrics.py
@@ -2,18 +2,17 @@ from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 from icon_metrics.config import settings
-
-# from icon_metrics.workers.supply_cron import get_supply
+from icon_metrics.workers.supply_cron import get_supply
 
 
 def test_api_get_supply(db: Session, client: TestClient, run_process_wait):
-    # with db as session:
-    # get_supply(session)
-    response = client.get(f"{settings.REST_PREFIX}/metrics/supply")
-    assert response.status_code == 200
-    supply = response.json()
-    assert isinstance(supply, dict)
-    assert supply["circulating_supply"] > 1e24
+    with db as session:
+        get_supply(session)
+        response = client.get(f"{settings.REST_PREFIX}/metrics/supply")
+        assert response.status_code == 200
+        supply = response.json()
+        assert isinstance(supply, dict)
+        assert supply["circulating_supply"] > 1e24
 
 
 # def test_api_get_node_state(db: Session, client: TestClient):
